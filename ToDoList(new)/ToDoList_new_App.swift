75492c39cc232bacb5f293f724aa3750ca54afd5
Miley <cutegoldendragon@gmail.com>
2025-07-25 13:31:44 -0400

    Initial Commit

---
+++ ToDoList(new)/ToDoList_new_App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_new_App.swift
+//  ToDoList(new)
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_new_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
